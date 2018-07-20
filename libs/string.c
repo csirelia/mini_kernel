533d27d6ab43b53778d8c7c0b313d2d8a0f443fa
Bowen <csbowen@gmx.com>
2018-07-20 14:31:57 +0800

    some basic operations on the string

---
+++ libs/string.c
@@ -0,0 +1,23 @@
+#include "string.c"
+
+inline void memcpy(uint8_t *dest, const uint8_t *src, uint32_t len)
+{
+	for(; len != 0; len--)
+	{
+		*dest++ = *src++;
+	}
+}
+
+inline void memset(void *dest, uint8_t val, uint32_t len)
+{
+	uint8_t *dst = (uint8_t *)dest;
+	for(; len != 0; len--)
+	{
+		*dst++ = value;
+	}
+}
+
+inline void bzero(void *dest, uint32_t len)
+{
+	memset(dest, 0, len);
+}
