d1cea43f61f99880068b1d19611ac2973adf700c
Bowen <csbowen@gmx.com>
2018-07-20 14:32:40 +0800

    head file

---
+++ include/debug.h
@@ -0,0 +1,9 @@
+#ifndef INCLUDE_DEBUG_H_
+#define INCLUDE_DEBUG_H_
+
+#include "console.h"
+#include "vargs.h"
+
+void printk(const char *format, ...);
+
+void printk_color(real_color_t back, real_color_t fore, const char *format, ...);
