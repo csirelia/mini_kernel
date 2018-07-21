1b8bcf00afd9797fc45c1b752c19aab7e972e724
csirelia <39493662+csirelia@users.noreply.github.com>
2018-07-21 11:35:03 +0800

    Update elf.c

--- libs/elf.c
+++ libs/elf.c
@@ -1,21 +1,3 @@
-/*
- * =====================================================================================
- *
- *       Filename:  elf.c
- *
- *    Description:  ELF 格式解析相关函数
- *
- *        Version:  1.0
- *        Created:  2013年11月06日 12时51分24秒
- *       Revision:  none
- *       Compiler:  gcc
- *
- *         Author:  Hurley (LiuHuan), liuhuan1992@gmail.com
- *        Company:  Class 1107 of Computer Science and Technology
- *
- * =====================================================================================
- */
-
 #include "common.h"
 #include "string.h"
 #include "elf.h"
